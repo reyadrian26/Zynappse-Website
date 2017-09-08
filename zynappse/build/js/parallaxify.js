91a06894b9af35a73522ccf0daf5a09f40dad686
Marvie Ellaine C Alar <marvieellainecalar@BatGirl.local>
2017-09-08 13:11:23 +0800

    -Added build folder

---
+++ zynappse/build/js/parallaxify.js
@@ -0,0 +1,34 @@
+// Created by Ali Torbati
+// ali.torbati@gmail.com
+// 2015
+
+(function ($) {
+
+  $.fn.parallaxifyfixed = function() {
+
+    return this.each(function() {
+      var $element = $(this);
+
+      function percentageSeen () {
+        var viewportHeight = $(window).height(),
+            winScrollTop = $(window).scrollTop(),
+            elementOffsetTop = $element.offset().top,
+            elementHeight = $element.height();
+
+        var distance = (winScrollTop + viewportHeight) - elementOffsetTop;
+        var percentage = distance / ((viewportHeight + elementHeight) / 100);
+
+        if (percentage < 0) return 0;
+        else if (percentage > 100) return 100;
+        else return percentage;
+      }
+
+      $element.css({ 'background-position-y' : percentageSeen()+'%' });
+
+      $(window).on('scroll', function() {
+        $element.css({ 'background-position-y' : percentageSeen()+'%' });
+      });
+    });
+  };
+
+}(jQuery));
