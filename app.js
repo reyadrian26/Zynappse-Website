233f9afaba2407841723af62fa07e4dcfd22323c
Edmhar Fabros <edmhar.fabros@zynappse.com>
2018-01-05 13:48:18 +0800

    change routing

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
+
+
 
 
 
@@ -23,7 +25,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
 app.use('/',express.static(path.join(__dirname, '/zynappse/build')));
+
 console.log('static file is: ',path.join(__dirname, '/zynappse/build'));
 
 app.use('/', index);
