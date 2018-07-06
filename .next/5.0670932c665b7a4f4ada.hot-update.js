f0765df03381200dbb1d579b5f8d8ac9416e0ac3
Edmhar Fabros <edmhar.fabros@zynappse.com>
2018-07-06 15:08:34 +0800

    - remove hot update

--- .next/5.0670932c665b7a4f4ada.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(5,[])
-//# sourceMappingURL=5.0670932c665b7a4f4ada.hot-update.js.map
--- .next/5.da4f72b703173a856e1f.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(5,[])
-//# sourceMappingURL=5.da4f72b703173a856e1f.hot-update.js.map
