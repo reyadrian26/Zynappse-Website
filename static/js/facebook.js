09fa2aba75d0dc28105c51ae8979ea21070bac25
Zynappse Trainee <bryan.robles@zynappse.com>
2018-08-10 17:58:06 +0800

    Changed .net to .com

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -11,6 +11,6 @@ window.fbAsyncInit = function() {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  js.src = 'https://connect.facebook.com/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
