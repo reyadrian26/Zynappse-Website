09089d3d6b27a658fc843d2816cc89bb9e3b8e76
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-05-06 11:58:54 +0800

    feat: add (Footer component, sass file, and image asset)

---
+++ components/Footer/Footer.js
@@ -0,0 +1,35 @@
+/** COMPONENTS */
+import { getLocalizedString } from "strings";
+import Navbar from "../Navbar/Navbar";
+/** SEMANTIC UI COMPONENTS */
+import {
+  Container,
+  Header,
+  MenuItem,
+  Menu,
+  Segment,
+  Button,
+  Grid,
+  GridRow,
+  GridColumn,
+  ListItem,
+  List,
+  Icon
+  //   Image
+} from "components/generics/SemanticUIReact";
+
+/** STYLES */
+import style from "../../styles/Footer.module.scss";
+
+import footerBackground from "../../assets/FooterImage.png";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className={style.footerContainer}>
+      
+    </div>
+  );
+};
+
+export default Footer;
