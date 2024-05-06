3a42797d7bf86fced548c2407ef44645f89635b6
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-05-06 11:35:47 +0800

    Feat: update (Semantic UI React imports)

--- components/generics/SemanticUIReact.js
+++ components/generics/SemanticUIReact.js
@@ -5,9 +5,31 @@ import dynamic from "next/dynamic";
 const Container = dynamic(() => import("semantic-ui-react").then(({ Container }) => Container));
 const Header = dynamic(() => import("semantic-ui-react").then(({ Header }) => Header));
 const Loader = dynamic(() => import("semantic-ui-react").then(({ Loader }) => Loader));
+const MenuItem = dynamic(() => import("semantic-ui-react").then(({ MenuItem }) => MenuItem));
+const Menu = dynamic(() => import("semantic-ui-react").then(({ Menu }) => Menu));
+const Segment = dynamic(() => import("semantic-ui-react").then(({ Segment }) => Segment));
+const Button = dynamic(() => import("semantic-ui-react").then(({ Button }) => Button));
+const GridRow = dynamic(() => import("semantic-ui-react").then(({ GridRow }) => GridRow));
+const GridColumn = dynamic(() => import("semantic-ui-react").then(({ GridColumn }) => GridColumn));
+const Grid = dynamic(() => import("semantic-ui-react").then(({ Grid }) => Grid));
+const Image = dynamic(() => import("semantic-ui-react").then(({ Image }) => Image));
+const Icon = dynamic(() => import("semantic-ui-react").then(({ Icon }) => Icon));
+const ListItem = dynamic(() => import("semantic-ui-react").then(({ ListItem }) => ListItem));
+const List = dynamic(() => import("semantic-ui-react").then(({ List }) => List));
 
 export {
     Container,
     Header,
     Loader,
+    MenuItem,
+    Menu,
+    Segment,
+    Button,
+    GridRow,
+    GridColumn,
+    Grid,
+    Image,
+    ListItem, 
+    List,
+    Icon
 };
