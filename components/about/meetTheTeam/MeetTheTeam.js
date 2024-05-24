9ae1f18cc7123993a11aac3b472d299ca086592e
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-05-24 16:50:56 +0800

    feat: update (meetTheTeam components and scss files)

---
+++ components/about/meetTheTeam/MeetTheTeam.js
@@ -0,0 +1,60 @@
+// src/components/MeetTheTeam/MeetTheTeam.js
+
+import React, { useState, useEffect } from "react";
+import style from "./MeetTheTeam.module.scss";
+import Image from "next/future/image";
+import { useInView } from "react-intersection-observer";
+import { Container, Header } from "components/generics/SemanticUIReact";
+import { imageSources } from "./MeetTheTeamData";
+
+const MeetTheTeam = () => {
+  const { ref: headerRef, inView: myHeaderIsVisible } = useInView();
+  const [animatedHeaderOnce, setAnimatedHeaderOnce] = useState(false);
+  const [animateImages, setAnimateImages] = useState(false);
+  const [clickedImages, setClickedImages] = useState(false);
+
+  useEffect(() => {
+    if (myHeaderIsVisible && !animatedHeaderOnce) {
+      setAnimatedHeaderOnce(true);
+    }
+  }, [myHeaderIsVisible, animatedHeaderOnce]);
+
+  const handleMouseEnter = () => {
+    setAnimateImages(true);
+  };
+
+  const handleContainerClick = () => {
+    setClickedImages(true);
+  };
+
+  return (
+    <>
+      <Container className="meetTheTeamContainer" onClick={handleContainerClick}>
+        <div ref={headerRef}>
+          <Header
+            as="h1"
+            content="MEET THE TEAM"
+            className={`${style.header} ${animatedHeaderOnce ? style.animateHeader : ""}`}
+          />
+          <Header
+            as="h3"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            className={`${style.subheader} ${animatedHeaderOnce ? style.animateHeader : ""}`}
+          />
+        </div>
+        <Container className="employeesContainer" onMouseEnter={handleMouseEnter}>
+          {imageSources.map((image, index) => (
+            <Image
+              key={index}
+              src={image.src}
+              className={`employeesImage ${image.className} ${image.animateCondition === "animateImages" && animateImages ? image.animateClass : ""} ${image.animateCondition === "clickedImages" && clickedImages ? image.animateClass : ""}`}
+              layout="raw"
+            />
+          ))}
+        </Container>
+      </Container>
+    </>
+  );
+};
+
+export default MeetTheTeam;
---
+++ components/about/meetTheTeam/MeetTheTeamData.js
@@ -0,0 +1,25 @@
+// src/components/MeetTheTeam/MeetTheTeamData.js
+
+import EmployeePlaceholder from "../../../public/about/EmployeePlaceholder.png";
+import EmployeePlaceholder2 from "../../../public/about/EmployeePlaceholder-2.png";
+import EmployeePlaceholder3 from "../../../public/about/EmployeePlaceholder-3.png";
+import EmployeePlaceholder4 from "../../../public/about/EmployeePlaceholder-4.png";
+import EmployeePlaceholder5 from "../../../public/about/EmployeePlaceholder-5.png";
+
+export const imageSources = [
+  { src: EmployeePlaceholder, className: "image-1", animateClass: "animateImage1", animateCondition: "animateImages" },
+  { src: EmployeePlaceholder2, className: "image-2", animateClass: "animateImage2", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder3, className: "image-3", animateClass: "animateImage3", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder4, className: "image-4", animateClass: "animateImage4", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder5, className: "image-5", animateClass: "animateImage5", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder4, className: "image-6", animateClass: "animateImage6", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder3, className: "image-7", animateClass: "animateImage7", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder, className: "image-8", animateClass: "animateImage8", animateCondition: "animateImages" },
+  { src: EmployeePlaceholder2, className: "image-9", animateClass: "animateImage9", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder5, className: "image-10", animateClass: "animateImage10", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder5, className: "image-11", animateClass: "animateImage11", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder4, className: "image-12", animateClass: "animateImage12", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder3, className: "image-13", animateClass: "animateImage13", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder2, className: "image-14", animateClass: "animateImage14", animateCondition: "clickedImages" },
+  { src: EmployeePlaceholder, className: "image-15", animateClass: "animateImage15", animateCondition: "animateImages" },
+];
