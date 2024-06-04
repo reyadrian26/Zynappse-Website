2a3daceffab9952eaa0333b5802b10411cd08ca1
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-06-04 09:29:14 +0800

    feat: update (updated pages component)

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navbar from "../components/about/navbar/Navbar";
+import React, { useState, useEffect } from 'react';
+import Navbar from "../components/home/navbar/Navbar";
 import Hero from "../components/about/heroSection/HeroSectionAbout";
 import Footer from "../components/about/footer/Footer";
 import WhoIsZynappse from "../components/about/whoIsZynappse/WhoIsZynappse";
@@ -8,21 +8,25 @@ import MeetTheTeam from "../components/about/meetTheTeam/MeetTheTeam";
 import OurStory from "../components/about/OurStory/OurStory";
 import Achievements from "../components/about/achievements/Achievements";
 import ScrollToTop from "react-scroll-to-top";
-
+import ScrollToTopStyle from "../components/scrollToTop/ScrollToTop.module.scss"
 
 const About = () => {
-
   return (
     <>
-        <Navbar />
-        <Hero />
-        <WhoIsZynappse/>
-        <Executives />
-        <MeetTheTeam />
-        <OurStory />
-        <Achievements />
-        <ScrollToTop smooth viewBox="0 0 47 46" svgPath="M 45.25269317626953 39 L 1.747308135032654 39 L 23.5 1.974141478538513 L 45.25269317626953 39 Z M 23.5 3.948284149169922 L 3.494617462158203 38 L 43.5053825378418 38 L 23.5 3.948284149169922 M 23.5 0 L 47 40 L 0 40 L 23.5 0 Z"/>
-        <Footer />
+      <Navbar />
+      <Hero />
+      <WhoIsZynappse />
+      <Executives />
+      <MeetTheTeam />
+      <OurStory />
+      <Achievements />
+      <ScrollToTop 
+        className={ScrollToTopStyle.scrollToTop}
+        smooth 
+        viewBox="0 0 47 46" 
+        svgPath="M 45.25269317626953 39 L 1.747308135032654 39 L 23.5 1.974141478538513 L 45.25269317626953 39 Z M 23.5 3.948284149169922 L 3.494617462158203 38 L 43.5053825378418 38 L 23.5 3.948284149169922 M 23.5 0 L 47 40 L 0 40 L 23.5 0 Z"
+      />
+      <Footer />
     </>
   );
 };
